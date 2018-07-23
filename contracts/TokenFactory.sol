e91c6151b9706abccb9a9af2723be31bb53e17d9
Bekket <jackatwork@yandex.ru>
2018-07-23 17:02:14 +0300

    Token2: returned of the King

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -1,27 +1,40 @@
 pragma solidity ^0.4.24;
  import "./ST.sol";
-//import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
-//import "./StandardToken.sol";
-//import "./Token.sol";
+
+import "./Token.sol";
 
 
 contract TokenFactory {
-    address dao;
+    address god;
     mapping (address => address[]) public tokens;
 
     constructor() public {
         dao = msg.sender;
     }
 
-    modifier onlyDao(){
-        require(msg.sender != dao);
+    modifier onlyGod(){
+        require(msg.sender != god);
         _;
     }
 
     event TokenCreated(address owner, address token);
 
-    function changeAdresses(address _dao) public onlyDao {
-        dao = _dao;
+    function changeAdresses(address _god) public onlyGod {
+        god = _god;
+    }
+
+    function createSToken(string _name,
+    string _symbol,
+    uint8 _decimals,
+    uint _INITIAL_SUPPLY) public returns(address) {
+    address token = 0x0;
+
+          token = address(new ST(_name,_symbol,_decimals,_INITIAL_SUPPLY));
+
+    //    tokens[msg.sender].push(token);
+        emit TokenCreated(msg.sender, token);
+       return token;
+
     }
 
     function createToken(string _name,
@@ -29,14 +42,14 @@ contract TokenFactory {
     uint8 _decimals,
     uint _INITIAL_SUPPLY) public returns(address) {
     address token = 0x0;
-    //    token = address(new Token(_name, _symbol, _decimals, _INITIAL_SUPPLY, msg.sender));
-          token = address(new ST(_name,_symbol,_decimals,_INITIAL_SUPPLY));
-  //  token = new ST(_name,_symbol,_decimals,_INITIAL_SUPPLY);
-    //    tokens[msg.sender].push(token);
+        token = address(new Token(_name, _symbol, _decimals, _INITIAL_SUPPLY, msg.sender));
+
+        tokens[msg.sender].push(token);
         emit TokenCreated(msg.sender, token);
        return token;
 
     }
+
 
     function getTokens(address _owner) view public returns(address[]) {
         return tokens[_owner];
