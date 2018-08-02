5012a1cd373940456d32ceaafaf55a74a30140be
Bekket <jackatwork@yandex.ru>
2018-08-02 19:41:17 +0300

    before isolation

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -2,11 +2,14 @@ pragma solidity ^0.4.24;
 
 
 import "./Token.sol";
+import "zeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 
 
 contract TokenFactory {
     address god;
     mapping (address => address[]) public tokens;
+    // define zheton as Token contract. There is might be a pitfall, cause crowdsale await ERC20
+  //  Token public zheton;
 
     constructor() public {
         god = msg.sender;
@@ -22,63 +25,51 @@ contract TokenFactory {
     function changeAdresses(address _god) public onlyGod {
         god = _god;
     }
-/*
-    function createSToken(string _name,
-    string _symbol,
-    uint8 _decimals,
-    uint _INITIAL_SUPPLY) public returns(address) {
-    address token = 0x0;
-
-          token = address(new ST(_name,_symbol,_decimals,_INITIAL_SUPPLY));
-
-    //    tokens[msg.sender].push(token);
-        emit TokenCreated(msg.sender, token);
-       return token;
-
-    }
-*/
-
-/*  Appendix
-
-    function createCrowdsaleToken(){
 
 
 
-    }
-*/
+
+
 
 // Function that create Token, crowdsale contract and start it at once
 // Note that it can be burn a lot of gas, so I think we need to split this function in the future
-function createToken(string _name,
-string _symbol,
-
-uint _INITIAL_SUPPLY) public returns(address) {
-address token = 0x0;
-    token = address(new Token(_name, _symbol, _INITIAL_SUPPLY, msg.sender));
-
-    tokens[msg.sender].push(token);
-    emit TokenCreated(msg.sender, token);
-   return token;
-
-}
+     function createCrowdsaleToken(string _name,
+     string _symbol,
+     uint _INITIAL_SUPPLY, uint256 _rate, address _wallet) public{
+    //   Token tok = createToken(_name,_symbol,_INITIAL_SUPPLY);
+       Token tok = Token(new Token(_name, _symbol, _INITIAL_SUPPLY, msg.sender));
+       address crd = createCrowdsale(_rate,_wallet,tok);
 
 
 
 
-/* function that create Simple Token without crowdsale
+   }
+
+// Function that creates crowdsale with given parameters
+    function createCrowdsale(uint256 _rate, address _wallet, Token _token) public returns(address) {
+      address crowdsale = 0x0;
+      crowdsale = address(new Crowdsale(_rate,_wallet,_token));
+      return crowdsale;
+
+
+    }
+
+
+// function that create Simple Token without crowdsale
+//  this functiuon returns ADDRESS type
     function createToken(string _name,
     string _symbol,
 
     uint _INITIAL_SUPPLY) public returns(address) {
-    address token = 0x0;
-        token = address(new Token(_name, _symbol, _INITIAL_SUPPLY, msg.sender));
+  //  address token = 0x0;
+      address token = address(new Token(_name, _symbol, _INITIAL_SUPPLY, msg.sender));
 
         tokens[msg.sender].push(token);
         emit TokenCreated(msg.sender, token);
        return token;
 
     }
-*/
+
 
     function getTokens(address _owner) view public returns(address[]) {
         return tokens[_owner];
