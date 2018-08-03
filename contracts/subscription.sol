8fc58ac7212a36a78d7bf54d4c8f92b191eaaf6d
Bekket <jackatwork@yandex.ru>
2018-08-03 15:47:06 +0300

    basic subscription

---
+++ contracts/subscription.sol
@@ -0,0 +1,48 @@
+/* Contract of basica subscription
+
+*/
+
+pragma solidity ^0.4.24;
+
+import "./Token.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract Subsctiption is Ownable {
+
+  Token token;
+  mapping (address => uint) public users;
+  address crowdsale;
+
+  constructor(address _token, address _owner, address _crowdsale) public {
+
+    token = Token(_token);
+    owner = _owner;
+    crowdsale = _crowdsale;
+
+
+  }
+
+  function signUp(){
+
+    uint am = 1 * 1 ether;
+    token.transferFrom(msg.sender,this,am);
+    users[msg.sender]=am;
+  }
+
+  function signOut(){
+    uint am = 1 * 1 ether;
+    token.transfer(msg.sender,am);
+    users[msg.sender]=0;
+
+  }
+
+  function banUser(address _user){
+
+    uint am = 1 * 1 ether;
+    token.transfer(crowdsale,am);
+    users[_user]=0;
+
+  }
+
+
+}
