97177a927201185ae2f9e3e82c6ceb49266fab55
Bekket <jackatwork@yandex.ru>
2018-08-03 16:11:31 +0300

    README upd

--- contracts/subscription.sol
+++ contracts/subscription.sol
@@ -13,6 +13,10 @@ contract Subscription is Ownable {
   mapping (address => uint) public users;
   address crowdsale;
 
+  event signedUp(address who);
+  event signedOut(address who);
+  event banned(address who);
+
   constructor(address _token, address _owner, address _crowdsale) public {
 
     token = Token(_token);
@@ -27,13 +31,14 @@ contract Subscription is Ownable {
     uint am = 1 * 1 ether;
     token.transferFrom(msg.sender,this,am);
     users[msg.sender]=am;
+    signedUp(msg.sender);
   }
 
   function signOut() public{
     uint am = 1 * 1 ether;
     token.transfer(msg.sender,am);
     users[msg.sender]=0;
-
+    signedOut(msg.sender);
   }
 
   function banUser(address _user) public onlyOwner {
@@ -41,6 +46,7 @@ contract Subscription is Ownable {
     uint am = 1 * 1 ether;
     token.transfer(crowdsale,am);
     users[_user]=0;
+    banned(msg.sender);
 
   }
 
