2bdf70420f93f00b41c1fddf5d9cf0e7e7762a3c
Bekket <jackatwork@yandex.ru>
2018-08-18 16:32:42 +0300

    Idea

---
+++ contracts/Antispam.sol
@@ -0,0 +1,7 @@
+/*
+
+Antispam idea in such version of subscription where subscriber
+adding deposit to the channel and get him back when unsubscribe.
+
+This version is work with 'ether', not tokens.
+*/
