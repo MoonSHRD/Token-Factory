e9ac62b29155e68b199732539f5f35fefe89e815
Bekket <jackatwork@yandex.ru>
2018-09-17 17:23:15 +0300

    bounty hanter

--- contracts/Prize.sol
+++ contracts/Prize.sol
@@ -6,10 +6,37 @@ pragma solidity ^0.4.24;
 */
 
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
 import "./Token.sol";
 
-contract Prize {
+contract Prize is Ownable {
 
+  Token token;
+  // sector Priz na barabane
+  uint bounty;
 
+  constructor(address _token,address _owner) public {
+    token = Token(_token);
+    owner = _owner;
+  }
+
+  function givePrize() payable public onlyOwner {
+    bounty += msg.value;
+  }
+
+// bounty hanter.. BOUNTY HANTER!
+  function getPrize(uint _value) public {
+    uint sup = token.totalSupply();
+    uint tokenshare = SafeMath.div(sup,100);
+    uint multiplex = SafeMath.div(tokenshare,_value);
+    token.transferFrom(msg.sender,owner,_value);
+    require(tokenshare >= 1);
+    uint moneyshare = SafeMath.div(bounty,100);
+    uint cashout = SafeMath.mul(moneyshare,multiplex);
+    address reciver = msg.sender;
+  //  require(reciver.call.value(cashout));
+    reciver.transfer(cashout);
+
+  }
 
 }
