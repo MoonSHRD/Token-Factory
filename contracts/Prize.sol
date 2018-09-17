3dc1b00e11fbecaff478901208c2d66b51246b3d
Bekket <jackatwork@yandex.ru>
2018-09-17 16:53:54 +0300

    prize boilerplate

---
+++ contracts/Prize.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.24;
+
+/*
+    template for crowdsale ovveride
+
+*/
+
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./Token.sol";
+
+contract Prize {
+
+
+
+}
