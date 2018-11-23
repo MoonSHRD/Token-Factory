fd1063808af7c7d30db042fec02d1f802205820b
dimakaplin <35988432+dimakaplin@users.noreply.github.com>
2018-11-23 11:14:17 +0300

    Update test.sh

--- .bin/test.sh
+++ .bin/test.sh
@@ -25,7 +25,7 @@ ganache_running() {
 
 start_ganache() {
   # We define 10 accounts with balance 1M ether, needed for high-value tests.
-  local accounts={
+  local accounts=(
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200,1000000000000000000000000"
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501201,1000000000000000000000000"
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501202,1000000000000000000000000"
@@ -36,7 +36,7 @@ start_ganache() {
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501207,1000000000000000000000000"
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501208,1000000000000000000000000"
     --account="0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501209,1000000000000000000000000"
-  }
+  )
 
   if [ "$SOLIDITY_COVERAGE" = false ]; then
     node_modules/.bin/testrpc-sc --gasLimit 0xfffffffffff --port "$ganache_port" "${accounts[@]}" > /dev/null &
